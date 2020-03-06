fe9fd7385a7d1f28736599d8843339141914ff75
kevin-ghannoum <kevin.ghannoum@gmail.com>
2020-03-05 23:13:38 -0500

    #30 Created a Follow model and a follow button.

--- campfire/user/models.py
+++ campfire/user/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
 class Post(models.Model):
-    user_name = models.CharField(max_length=31)
+    username = models.CharField(max_length=31)
     image = models.ImageField(upload_to='images/')
     caption = models.CharField(max_length=255)
     upload_time = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
-    user_name = models.CharField(max_length=31)
+    username = models.CharField(max_length=31)
     comment = models.CharField(max_length=255)
     upload_time = models.DateTimeField(auto_now=True)
     post_key = models.IntegerField()
+
+class Follow(models.Model):
+    username = models.CharField(max_length=31)
+    following = models.CharField(max_length=31)
