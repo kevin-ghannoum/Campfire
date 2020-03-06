7cab6e655bff47af2951c7c851fb0c30f9a950d7
pascwhale <pascal.demerdjian@gmail.com>
2020-03-06 12:22:47 -0500

    #37 Added a test case for user app.

--- campfire/user/tests.py
+++ campfire/user/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class test(TestCase):
+    def test(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEquals(response.status_code, 200)
